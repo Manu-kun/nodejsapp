b6e1e2ff4beab82612764a16cd8a266992c3f9c7
Manukun <71195299+Manu-kun@users.noreply.github.com>
2025-09-09 13:41:12 +0200

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/Manu-kun/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('Manu-kun')
+            node / gitConfigEmail('mbaenamira000@gmail.com')
         }
     }
     triggers {
